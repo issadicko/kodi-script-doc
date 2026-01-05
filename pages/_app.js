917ae9713c67e200a13c9dff21b1b943b09d5b92
dickode78 <dickode78@gmail.com>
2026-01-05 13:18:20 +0000

    feat: implement Google Analytics tracking by adding `@next/third-parties` and integrating the `GoogleAnalytics` component.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles.css'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Component {...pageProps} />
+            <GoogleAnalytics gaId="G-5PH4ZZ20BD" />
+        </>
+    )
 }
